93ea7a19e53b0e7de08489fca696fbf78fb587f2
it <yeongbin0909@naver.com>
2025-03-24 08:34:13 +0900

    code_read_pointer 와 관련 개념 제거

--- py_hambugi/interpreter.py
+++ py_hambugi/interpreter.py
@@ -1,17 +1,11 @@
+from .commands.abstract_command import AbstractCommand
+
+
 class Interpreter:
     def __init__(self):
         self.raw_code: str = None
-        self.code_read_pointer: int = 0
-
-    def shift_pointer(self, pointer: int):
-        self.code_read_pointer += pointer
-
-    def seek(self): self.code_read_pointer = 0
-
+        self.commands: list[AbstractCommand] = None
 
     def set_code(self, raw_code: str):
         self.raw_code = raw_code.replace(" ", "").replace("\n", "")
-        self.seek()
-
-
-
+        self.commands = list()
