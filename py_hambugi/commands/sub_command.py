8519bcb080b6d4132487649ef1eb1c1e2fb357dd
it <yeongbin0909@naver.com>
2025-03-24 15:40:44 +0900

    뺄셈 명령어 추가

--- py_hambugi/commands/sub_command.py
+++ py_hambugi/commands/sub_command.py
@@ -5,34 +5,34 @@ from . import syntax_set
 from .value import Value
 
 
-class AddCommand(AbstractCommand):
-    def __init__(self, code: str, target_variable: Value, adding_value: Value):
+class SubCommand(AbstractCommand):
+    def __init__(self, code: str, target_variable: Value, sub_value: Value):
         self.code = code
         self.target_value: Value = target_variable
-        self.adding_value: Value = adding_value
+        self.sub_value: Value = sub_value
 
     def run(self, runtime_manager: "RuntimeManager") -> None:
         self.target_value.set_value(
             runtime_manager,
             self.target_value.get_value(runtime_manager)
-            + self.adding_value.get_value(runtime_manager)
+            - self.sub_value.get_value(runtime_manager)
         )
 
     @classmethod
     def from_code(cls, code: str) -> Self | None:
-        if code.startswith(syntax_set.ADD_COMMAND):
+        if code.startswith(syntax_set.SUB_COMMAND):
             read_code = ""
-            code, read_code = cls.read_code(code, read_code, syntax_set.ADD_COMMAND)
+            code, read_code = cls.read_code(code, read_code, syntax_set.SUB_COMMAND)
 
             target_value = Value.from_code(code)
             if target_value is None or not target_value.is_variable(): return None
             code, read_code = cls.read_code(code, read_code, target_value.code)
 
-            adding_value = Value.from_code(code)
-            if adding_value is None: return None
-            code, read_code = cls.read_code(code, read_code, adding_value.code)
+            sub_value = Value.from_code(code)
+            if sub_value is None: return None
+            code, read_code = cls.read_code(code, read_code, sub_value.code)
 
-            return cls(read_code, target_value, adding_value)
+            return cls(read_code, target_value, sub_value)
 
         else:
             return None
