847aea3539b4eade5f96620496c6be4ffdb5ff62
it <yeongbin0909@naver.com>
2025-03-24 09:59:16 +0900

    출력 커맨드 전체 코드

--- py_hambugi/commands/print_command.py
+++ py_hambugi/commands/print_command.py
@@ -1,16 +1,35 @@
 from typing import Self
 
+from . import syntax_set
 from .abstract_command import AbstractCommand, RuntimeManager
+
+from.value import Value
 
 
 
 class PrintCommand(AbstractCommand):
 
-    def __init__(self):
-        ...
+    def __init__(self, code: str, value: Value):
+        self.code = code
+        self.value = value
 
     def run(self, runtime_manager: "RuntimeManager") -> None:
-        pass
+        print(chr(self.value.get_value(runtime_manager)), end="")
 
+    @classmethod
     def from_code(cls, code: str) -> Self | None:
-        ...
+        read_code = ""
+
+        value = Value.from_code(code)
+        if value is None: return None
+
+        read_code += code[:len(value.code)]
+        code = code[len(value.code):]
+
+        if code.startswith(syntax_set.PRINT_COMMAND):
+            read_code += code[:6]
+            code = code[6:]
+
+            return cls(read_code, value)
+
+        else: return None
