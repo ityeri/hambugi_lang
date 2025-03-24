016609efbb006b9ec0ebfa41e83bd1339c946f78
it <yeongbin0909@naver.com>
2025-03-24 17:30:27 +0900

    테스트 내용 변경

--- command_test.py
+++ command_test.py
@@ -1,19 +1,17 @@
-from py_hambugi.commands import PrintCommand
+from py_hambugi.commands import WriteCommand
 from py_hambugi import RuntimeManager
-from py_hambugi.commands.value_copy_command import ValueCopyCommand
 
 runtime_manager = RuntimeManager()
 
-runtime_manager.a = 4
-runtime_manager.b = 5
-runtime_manager.c = 6
+runtime_manager.a = 2
+runtime_manager.b = 10
 
-code = "햄부기 햄부 햄북어"
+code = "햄부거 햄부 햄북어"
 
 code = code.replace(" ", "").replace("\n", "")
 
-value_copy_command = ValueCopyCommand.from_code(code)
+command = WriteCommand.from_code(code)
 
-value_copy_command.run(runtime_manager)
+command.run(runtime_manager)
 
 breakpoint()
