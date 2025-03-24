c99984caef8ff2b24216f5df57a33c01ade58f44
it <yeongbin0909@naver.com>
2025-03-24 15:38:09 +0900

    변수 지칭 용어 정정

---
+++ py_hambugi/commands/sub_command.py
@@ -0,0 +1,38 @@
+from typing import Self
+
+from .abstract_command import AbstractCommand, RuntimeManager
+from . import syntax_set
+from .value import Value
+
+
+class AddCommand(AbstractCommand):
+    def __init__(self, code: str, target_variable: Value, adding_value: Value):
+        self.code = code
+        self.target_value: Value = target_variable
+        self.adding_value: Value = adding_value
+
+    def run(self, runtime_manager: "RuntimeManager") -> None:
+        self.target_value.set_value(
+            runtime_manager,
+            self.target_value.get_value(runtime_manager)
+            + self.adding_value.get_value(runtime_manager)
+        )
+
+    @classmethod
+    def from_code(cls, code: str) -> Self | None:
+        if code.startswith(syntax_set.ADD_COMMAND):
+            read_code = ""
+            code, read_code = cls.read_code(code, read_code, syntax_set.ADD_COMMAND)
+
+            target_value = Value.from_code(code)
+            if target_value is None or not target_value.is_variable(): return None
+            code, read_code = cls.read_code(code, read_code, target_value.code)
+
+            adding_value = Value.from_code(code)
+            if adding_value is None: return None
+            code, read_code = cls.read_code(code, read_code, adding_value.code)
+
+            return cls(read_code, target_value, adding_value)
+
+        else:
+            return None
