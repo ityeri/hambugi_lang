4257ff3059fad4521dde8ac6fb958f515d530c37
it <yeongbin0909@naver.com>
2025-03-24 15:41:37 +0900

    빼기 명령어를 __init__ 에 추가

--- py_hambugi/commands/__init__.py
+++ py_hambugi/commands/__init__.py
@@ -2,10 +2,12 @@ from .abstract_command import AbstractCommand
 from .print_command import PrintCommand
 
 from .add_command import AddCommand
+from .sub_command import SubCommand
 
 __all__ = [
     "AbstractCommand",
     "PrintCommand",
 
-    "AddCommand"
+    "AddCommand",
+    "SubCommand"
 ]
