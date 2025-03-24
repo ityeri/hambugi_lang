2cf1dc8243e91ebb302d4f67d9b3c3c5428d1789
it <yeongbin0909@naver.com>
2025-03-24 10:15:15 +0900

    read_code 메서드가 code, read_code 를 반환하도록 수정

--- py_hambugi/commands/abstract_command.py
+++ py_hambugi/commands/abstract_command.py
@@ -20,8 +20,10 @@ class AbstractCommand(metaclass=ABCMeta):
     def from_code(cls, code: str) -> Self | None: ...
 
     @staticmethod
-    def read_code(code: str, read_code: str, readable_code: str):
+    def read_code(code: str, read_code: str, readable_code: str) -> tuple[str, str]:
         if not code.startswith(readable_code): raise ValueError("code 문자열이 readable_code 문자열로 시작하지 않습니다")
 
         read_code += code[:len(readable_code)]
         code = code[len(readable_code):]
+
+        return code, read_code
