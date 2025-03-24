03de696818bd87fbb612d8a6fb6fe02b95ec60d4
it <yeongbin0909@naver.com>
2025-03-24 09:57:34 +0900

    인터프리터 클래스에 command_set 을 통해 코드를 읽어들이는 코드 추가

--- py_hambugi/interpreter.py
+++ py_hambugi/interpreter.py
@@ -1,4 +1,5 @@
 from .commands.abstract_command import AbstractCommand
+from .command_sets import hambugi_lang
 
 
 class Interpreter:
@@ -9,3 +10,15 @@ class Interpreter:
     def set_code(self, raw_code: str):
         self.raw_code = raw_code.replace(" ", "").replace("\n", "")
         self.commands = list()
+
+        while len(self.raw_code) == 0:
+            for Command in hambugi_lang:
+
+                command = Command(raw_code)
+
+                if command is not None:
+
+                    self.raw_code = self.raw_code[:len(command.code)]
+                    self.commands.append(command)
+
+                    break
