588ab89a9159bad12d05a407bfd9544e3acc4d4b
it <yeongbin0909@naver.com>
2025-03-24 17:08:46 +0900

    hambugi_lang_command_set 로 리펙터링 및 누락된 명령어 클래스 추가

--- py_hambugi/command_sets.py
+++ py_hambugi/command_sets.py
@@ -1,7 +1,16 @@
 from typing import Type
 
 from .commands import *
+from .commands.value_copy_command import ValueCopyCommand
 
-hambugi_lang: list[Type[AbstractCommand]] = [
+hambugi_lang_command_set: list[Type[AbstractCommand]] = [
+    ValueCopyCommand,
+
+    AddCommand,
+    SubCommand,
+
+    ReadCommand,
+    WriteCommand,
+
     PrintCommand
 ]
